e21484bc98a6b9c570c5c070508aefc1812bd921
Dyelan6705 <Dylan.choukalos@gmail.com>
2026-03-02 13:09:56 -0600

    Update MCsim.py

--- MCsim.py
+++ MCsim.py
@@ -5,7 +5,7 @@ import yfinance as yf
 import datetime
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-from pandas_datareader import data as pdr
+
 
 def get_data(stocks, start, end):
     stockData = yf.download(stocks, start, end)
